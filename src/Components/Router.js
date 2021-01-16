907e15ead92cddd777aed60c51a1aa171e812977
hj lee <imhyojunglee@gmail.com>
2021-01-15 16:49:40 -0800

    use styled-components for Header-Component

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,17 +1,21 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 // import {HashRouter as Router, Route, Redirect} from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
